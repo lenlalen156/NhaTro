7c1b7c00d278dbe258dc7070ce9e367dab19b7f8
manh.nguyen <manh.nguyen@alobridge.com>
2022-06-18 11:35:25 +0700

    NhaTro: add base command

---
+++ app/Console/Commands/MakeService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Console\Commands;
+
+class MakeService extends BaseMakeFile
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:service {name}';
+    protected $description = 'Make service';
+    protected $folder = "Services";
+    protected $successMessage = "Create service success";
+
+    protected function generateContent(): string
+    {
+        return "<?php\n\nnamespace App\\{$this->folder};\n\nclass {$this->fileName}\n{\n}\n";
+    }
+}
